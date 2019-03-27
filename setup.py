64fba0982a318a9b47c983d6ccb243016dafb61f
Jirka Kremser <jkremser@redhat.com>
2019-03-27 15:03:06 +0100

    Fail with some sensible message when installing with pip (instead of pip3)

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 
 from setuptools import setup, find_packages
+import sys
+
+if sys.version_info[0] != 3:
+    sys.exit("\nPython3 is required in order to install soit")
 
 setup(
     packages=find_packages()
