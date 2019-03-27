5d35489163d16ddf6728894c62b799d8ee0d49ca
Jirka Kremser <jkremser@redhat.com>
2019-03-27 01:35:23 +0100

    setup

--- setup.py
+++ setup.py
@@ -3,8 +3,5 @@
 from setuptools import setup, find_packages
 
 setup(
-    packages=find_packages(),
-    entry_points = {
-        'console_scripts': ['soit=soit.main:check'],
-    }
+    packages=find_packages()
 )
